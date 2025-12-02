03d6fb9f8c679f00a6467c54ee923195f6fd6228
Irena <140869597+re-zero001@users.noreply.github.com>
2025-12-02 20:32:29 +0800

    Save deleted should exclude bots in groups

--- Telegram/SourceFiles/ayu/utils/telegram_helpers.cpp
+++ Telegram/SourceFiles/ayu/utils/telegram_helpers.cpp
@@ -609,6 +609,13 @@ bool isMessageSavable(const not_null<HistoryItem*> item) {
 	if (const auto possiblyBot = item->history()->peer->asUser()) {
 		return !possiblyBot->isBot() || (settings.saveForBots && possiblyBot->isBot());
 	}
+
+	const auto from = item->from();
+	const auto user = from ? from->asUser() : nullptr;
+	if (user && user->isBot()) {
+		return false;
+	}
+
 	return true;
 }
 
