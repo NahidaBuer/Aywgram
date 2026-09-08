fd77cefc36045b2272c7eeda0464b12fe495c26d
ZGX089 <159061718+ZG089@users.noreply.github.com>
2026-09-08 18:57:24 +0300

    feat(chats): reduce time taken to hold pfp to preview chat

--- Telegram/SourceFiles/window/window_chat_preview.cpp
+++ Telegram/SourceFiles/window/window_chat_preview.cpp
@@ -23,7 +23,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 namespace Window {
 namespace {
 
-constexpr auto kChatPreviewDelay = crl::time(1000);
+constexpr auto kChatPreviewDelay = crl::time(250);
 
 [[nodiscard]] bool SkipChatPreviewFor(Dialogs::Key key) {
 	const auto peer = key.peer();
