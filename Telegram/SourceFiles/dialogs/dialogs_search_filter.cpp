4a433374d15fd2c6b30b05ee5fc99adb60985ba4
Nahida <me@nahida.icu>
2026-09-09 13:58:40 +0800

    fix(search): show message type icons in filter menu

--- Telegram/SourceFiles/dialogs/dialogs_search_filter.cpp
+++ Telegram/SourceFiles/dialogs/dialogs_search_filter.cpp
@@ -10,20 +10,58 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "ui/widgets/popup_menu.h"
 
 #include "styles/style_dialogs.h"
+#include "styles/style_menu_icons.h"
 
 namespace Dialogs {
+namespace {
+
+[[nodiscard]] const style::icon &SearchFilterIcon(Api::SearchFilter filter) {
+	switch (filter) {
+	case Api::SearchFilter::NoFilter:
+		return st::menuIconShowAll;
+	case Api::SearchFilter::Photos:
+		return st::menuIconPhoto;
+	case Api::SearchFilter::Videos:
+		return st::dialogsSearchInMessageTypeVideo;
+	case Api::SearchFilter::Files:
+		return st::menuIconFile;
+	case Api::SearchFilter::Links:
+		return st::menuIconLink;
+	case Api::SearchFilter::Music:
+		return st::dialogsSearchInMessageTypeMusic;
+	case Api::SearchFilter::VoiceMessages:
+		return st::dialogsSearchInMessageTypeVoice;
+	case Api::SearchFilter::VideoMessages:
+		return st::dialogsSearchInMessageTypeRoundVideo;
+	case Api::SearchFilter::Gifs:
+		return st::menuIconGif;
+	case Api::SearchFilter::Polls:
+		return st::menuIconCreatePoll;
+	case Api::SearchFilter::MyMentions:
+		return st::dialogsSearchInMessageTypeMention;
+	case Api::SearchFilter::Locations:
+		return st::menuIconAddress;
+	case Api::SearchFilter::Pinned:
+		return st::menuIconPin;
+	}
+	Unexpected("SearchFilter in SearchFilterIcon.");
+}
+
+} // namespace
 
 void FillSearchFilterMenu(
 		not_null<Ui::PopupMenu*> menu,
 		Api::SearchFilter selected,
 		Fn<void(Api::SearchFilter)> callback) {
 	for (const auto filter : Api::SearchFilters()) {
-		const auto chosen = (filter == selected);
+		const auto icon = (filter == selected)
+			? &st::dialogsSearchInCheck
+			: &SearchFilterIcon(filter);
 		menu->addAction(
 			Api::SearchFilterLabel(filter),
 			[=] { callback(filter); },
-			chosen ? &st::dialogsSearchInCheck : nullptr,
-			chosen ? &st::dialogsSearchInCheck : nullptr);
+			icon,
+			icon);
 	}
 }
 
